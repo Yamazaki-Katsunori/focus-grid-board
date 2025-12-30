bc89393319774866c05537fd70614f2aa3757cdb
Yamazaki-katsunori <katunori0330@gmail.com>
2025-12-30 08:55:33 +0000

    chore commit: Laravel Sanctum の設定

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
